c6ee6e9ba2e7b7c54cdad0feaa608b4d18deb6d0
Chenx Dust <chenx_dust@outlook.com>
2024-10-01 17:14:06 +0800

    fix: Fix `tap-and-drag` typo.
    Signed-off-by: l1ttleflyyy <l1ttleflyyy@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -162,7 +162,7 @@ const struct option *gamescope_options = (struct option[]){
 	// Pointer options
 	// Libinput options
 	{ "tap-to-click", no_argument, nullptr, 0 },
-	{ "tap-to-drag", no_argument, nullptr, 0 },
+	{ "tap-and-drag", no_argument, nullptr, 0 },
 	{ "drag-lock", no_argument, nullptr, 0 },
 	{ "middle-emulation", no_argument, nullptr, 0 },
 	{ "disable-while-typing", no_argument, nullptr, 0 },
